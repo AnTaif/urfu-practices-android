96cf51cafd9ebac6ab87fdc35df1e16796362f2a
Mikhail Gavrilyak <gavrilyak.mikhail@gmail.com>
2025-10-19 17:38:34 +0500

    add network source for leaderboard viewmodel

--- app/src/main/java/github/antaif/urfupractices/di/MainModule.kt
+++ app/src/main/java/github/antaif/urfupractices/di/MainModule.kt
@@ -1,5 +1,8 @@
 package github.antaif.urfupractices.di
 
+import github.antaif.urfupractices.leaderboard.data.mapper.LeaderboardMapper
+import github.antaif.urfupractices.leaderboard.data.repository.LeaderboardRepository
+import github.antaif.urfupractices.leaderboard.domain.interactor.LeaderboardInteractor
 import github.antaif.urfupractices.leaderboard.presentation.viewModel.LeaderboardDriverDetailsViewModel
 import github.antaif.urfupractices.leaderboard.presentation.viewModel.LeaderboardViewModel
 import github.antaif.urfupractices.navigation.Route
@@ -11,6 +14,11 @@ import org.koin.dsl.module
 val mainModule = module {
     single { TopLevelBackStack<Route>(Routes.Leaderboard) }
 
-    viewModel { LeaderboardViewModel(get()) }
+    factory { LeaderboardMapper() }
+    single { LeaderboardRepository(get(), get()) }
+
+    single { LeaderboardInteractor(get()) }
+
+    viewModel { LeaderboardViewModel(get(), get()) }
     viewModel { LeaderboardDriverDetailsViewModel(get(), get()) }
 }
--- app/src/main/java/github/antaif/urfupractices/leaderboard/presentation/model/state/LeaderboardState.kt
+++ app/src/main/java/github/antaif/urfupractices/leaderboard/presentation/model/state/LeaderboardState.kt
@@ -3,6 +3,11 @@ package github.antaif.urfupractices.leaderboard.presentation.model.state
 import github.antaif.urfupractices.leaderboard.presentation.model.ui.LeaderboardUiModel
 
 data class LeaderboardState(
-    val leaderboard: LeaderboardUiModel
+    val state: State = State.Loading,
 ) {
+    sealed interface State {
+        object Loading : State
+        data class Error(val error: String) : State
+        data class Success(val data: LeaderboardUiModel) : State
+    }
 }
